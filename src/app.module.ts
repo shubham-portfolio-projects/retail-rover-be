7d547a94d793655fdd8175bd7fb451d5bd1420a0
Shubham Mathur <shubham.mathur.upwrk@gmail.com>
2024-05-14 11:23:32 +0530

    Create Users resource and setup mongoose and mongodb.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+ConfigModule.forRoot();
 @Module({
-  imports: [],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
